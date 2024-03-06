e678383af18a9cdf73d1395f0ca5791a8d688cfb
Simon <mayeung@student.42london.com>
2024-03-06 19:03:33 +0000

    fix get_user_input

--- srcs/get_user_input.c
+++ srcs/get_user_input.c
@@ -6,65 +6,56 @@
 /*   By: mayeung <mayeung@student.42london.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 14:22:58 by mayeung           #+#    #+#             */
-/*   Updated: 2024/03/06 13:20:09 by mayeung          ###   ########.fr       */
+/*   Updated: 2024/03/06 18:41:30 by mayeung          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int	ft_event(void)
+void	ft_init_get_user_input(char **old_line, int *parse_res)
 {
-	return (0);
-}
-
-int	ft_get_user_input(void)
-{
-	char	*line;
-	char	*old_line;
-	int		parse_res;
-
 	signal(SIGINT, &ft_signal_handler_waiting_input);
 	signal(SIGQUIT, SIG_IGN);
 	rl_event_hook = ft_event;
-	old_line = NULL;
-	parse_res = PARSE_OK;
+	if (old_line)
+		*old_line = NULL;
+	if (parse_res)
+		*parse_res = NO_PARSE_RESULT;
 	ft_vars()->break_readline = 0;
-	line = readline(PROMPT);
-	while (!ft_vars()->break_readline || parse_res == PARSE_OK)
+}
+
+void	ft_exit_shell(char **old_line)
+{
+	if (old_line)
+		free(*old_line);
+	ft_putstr_fd("exit\n", STDOUT_FILENO);
+	ft_free_res(1);
+	exit(ft_vars()->last_exe_res);
+}
+
+int	ft_process_line(char **line, char **old_line, int *parse_res)
+{
+	ft_trim_write_history(*line);
+	if (*old_line)
+		*line = ft_free_join_str(ft_strdup("\n"), *line);
+	*line = ft_free_join_str(*old_line, *line);
+	if (!(*line))
+		return (ALLOCATE_FAIL);
+	ft_vars()->toklist = ft_tokenize(*line);
+	if (!ft_vars()->toklist)
+		return (free(*old_line), free(*line), ALLOCATE_FAIL);
+	*parse_res = ft_parse_token(ft_vars()->toklist);
+	if (*parse_res == IMCOMPELETE_CMD)
 	{
-		if (!line && parse_res == PARSE_OK)
-		{
-			free(old_line);
-			ft_putstr_fd("exit\n", STDOUT_FILENO);
-			ft_free_res(1);
-			exit(ft_vars()->last_exe_res);
-		}
-		if (!line && parse_res == IMCOMPELETE_CMD)
-			return (ft_default_signal(),
-				ft_putstr_fd("unexpected EOF\n", STDERR_FILENO), PARSE_FAIL);
-		if (!ft_space_only(line) || parse_res == IMCOMPELETE_CMD)
-		{
-			ft_trim_write_history(line);
-			if (old_line)
-				line = ft_free_join_str(ft_strdup("\n"), line);
-			line = ft_free_join_str(old_line, line);
-			if (!line)
-				return (ALLOCATE_FAIL);
-			ft_vars()->toklist = ft_tokenize(line);
-			if (!ft_vars()->toklist)
-				return (free(line), ALLOCATE_FAIL);
-			parse_res = ft_parse_token(ft_vars()->toklist);
-			if (parse_res == PARSE_OK || parse_res == PARSE_FAIL)
-				break ;
-			old_line = line;
-			line = NULL;
-			ft_lstclear(&ft_vars()->toklist, &ft_free_token_node);
-		}
-		else if (ft_space_only(line) && parse_res == PARSE_OK)
-			return (ft_default_signal(), free(line), EMPTY_INPUT);
-		free(line);
-		line = readline(PROMPT_CON);
+		*old_line = *line;
+		*line = NULL;
+		ft_lstclear(&ft_vars()->toklist, &ft_free_token_node);
 	}
+	return (EXE_SUCCESS);
+}
+
+void	ft_cleanup(char *line, char *old_line, int parse_res)
+{
 	free(line);
 	if (ft_vars()->break_readline)
 	{
@@ -76,5 +67,32 @@ int	ft_get_user_input(void)
 		ft_lstclear(&ft_vars()->toklist, &ft_free_token_node);
 		ft_vars()->toklist = NULL;
 	}
+}
+
+int	ft_get_user_input(void)
+{
+	char	*line;
+	char	*old_line;
+	int		parse_res;
+
+	ft_init_get_user_input(&old_line, &parse_res);
+	line = readline(PROMPT);
+	while (!ft_vars()->break_readline
+		&& (parse_res == NO_PARSE_RESULT || parse_res == IMCOMPELETE_CMD))
+	{
+		if (!line && parse_res == NO_PARSE_RESULT)
+			ft_exit_shell(&old_line);
+		if (!line && parse_res == IMCOMPELETE_CMD)
+			return (ft_default_signal(),
+				ft_putstr_fd("unexpected EOF\n", STDERR_FILENO), PARSE_FAIL);
+		if ((!ft_space_only(line) || parse_res == IMCOMPELETE_CMD)
+			&& (ft_process_line(&line, &old_line, &parse_res)))
+			return (ALLOCATE_FAIL);
+		else if (ft_space_only(line) && parse_res == NO_PARSE_RESULT)
+			return (ft_default_signal(), free(line), EMPTY_INPUT);
+		if (parse_res == IMCOMPELETE_CMD)
+			line = readline(PROMPT_CON);
+	}
+	ft_cleanup(line, old_line, parse_res);
 	return (ft_default_signal(), parse_res);
 }
